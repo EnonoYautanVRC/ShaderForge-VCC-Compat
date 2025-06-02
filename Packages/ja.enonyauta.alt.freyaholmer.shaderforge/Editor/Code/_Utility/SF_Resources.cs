e3da8bc23446137ad50e11573a87b3250a2a15f8
yautaenon <kotetun2005@gmail.com>
2025-06-03 05:31:50 +0900

    Revert "fix: 🐛 put into proper working order"
    This reverts commit 18b896e1dae7bd8a2a0418e29760d78f60bb014c.

--- Packages/ja.enonyauta.alt.freyaholmer.shaderforge/Editor/Code/_Utility/SF_Resources.cs
+++ Packages/ja.enonyauta.alt.freyaholmer.shaderforge/Editor/Code/_Utility/SF_Resources.cs
@@ -2,11 +2,9 @@
 using UnityEditor;
 using System.Collections;
 
-namespace ShaderForge
-{
+namespace ShaderForge{
 
-	public static class SF_Resources
-	{
+	public static class SF_Resources {
 
 		public const string pInterface = "Interface/";
 		public const string pFonts = pInterface + "Fonts/";
@@ -14,47 +12,36 @@ namespace ShaderForge
 		public const string pGpuRendering = "GPU Rendering/";
 
 
-		public static T Load<T>(string name) where T : UnityEngine.Object
-		{
-			return (T)AssetDatabase.LoadAssetAtPath(InternalResourcesPath + name, typeof(T));
+		public static T Load<T>(string name) where T : UnityEngine.Object {
+			return (T)AssetDatabase.LoadAssetAtPath(InternalResourcesPath + name, typeof(T) );
 		}
 
-		public static UnityEngine.Object[] LoadAll(string name)
-		{
-			return AssetDatabase.LoadAllAssetsAtPath(InternalResourcesPath + name);
+		public static UnityEngine.Object[] LoadAll(string name) {
+			return AssetDatabase.LoadAllAssetsAtPath(InternalResourcesPath + name );
 		}
 
-		public static Texture2D LoadInterfaceIcon(string name)
-		{
+		public static Texture2D LoadInterfaceIcon(string name){
 			string path = InternalResourcesPath + "Interface/" + name;
-			Texture2D retTex = (Texture2D)AssetDatabase.LoadAssetAtPath(path + ".png", typeof(Texture2D));
-			if (retTex == null)
-			{
-				retTex = (Texture2D)AssetDatabase.LoadAssetAtPath(path + ".tga", typeof(Texture2D));
+			Texture2D retTex = (Texture2D)AssetDatabase.LoadAssetAtPath(path + ".png", typeof(Texture2D) );
+			if(retTex == null){
+				retTex = (Texture2D)AssetDatabase.LoadAssetAtPath(path + ".tga", typeof(Texture2D) );
 			}
 			return retTex;
 		}
 
-		public static Texture2D LoadNodeIcon(string name)
-		{
-			return (Texture2D)AssetDatabase.LoadAssetAtPath(InternalResourcesPath + "Interface/Nodes/" + name + ".png", typeof(Texture2D));
+		public static Texture2D LoadNodeIcon(string name){
+			return (Texture2D)AssetDatabase.LoadAssetAtPath(InternalResourcesPath + "Interface/Nodes/" + name + ".png", typeof(Texture2D) );
 		}
-
+	
 
 		private static string internalResourcesPath = "";
-		public static string InternalResourcesPath
-		{
-			get
-			{
-				if (string.IsNullOrEmpty(internalResourcesPath))
-				{
+		public static string InternalResourcesPath{
+			get{
+				if(string.IsNullOrEmpty(internalResourcesPath)){
 					string path;
-					if (SearchForInternalResourcesPath(out path))
-					{
+					if(SearchForInternalResourcesPath(out path)){
 						internalResourcesPath = path;
-					}
-					else
-					{
+					} else {
 						Debug.LogError("Unable to locate the internal resources folder. Make sure your Shader Forge installation is intact");
 						SF_Editor.instance.Close();
 					}
@@ -65,24 +52,20 @@ namespace ShaderForge
 
 
 
-		private static bool SearchForInternalResourcesPath(out string path)
-		{
+		private static bool SearchForInternalResourcesPath( out string path ){
 			path = "";
-			string partialPath = ".shaderforge/Editor/InternalResources/";
+			string partialPath = "/ShaderForge/Editor/InternalResources/";
 			string foundPath = null;
-			foreach (string s in AssetDatabase.GetAllAssetPaths())
-			{
-				if (s.Contains(partialPath))
-				{
+			foreach(string s in AssetDatabase.GetAllAssetPaths()){
+				if(s.Contains(partialPath)){
 					foundPath = s;
 					break;
 				}
 			}
-			if (foundPath == null)
-			{
+			if(foundPath == null){
 				return false;
 			}
-			string[] split = foundPath.Replace(partialPath, "#").Split('#');
+			string[] split = foundPath.Replace(partialPath,"#").Split('#');
 			path = split[0] + partialPath;
 			return true;
 		}
